f76a31ead43de75c598e5327eb926ae982c30727
jdbuckler <j.d.buckler@ieee.org>
2020-09-16 17:03:33 -0300

    few ityems after Raindrops exercism

--- test.go
+++ test.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "strconv"
+)
+
+func Convert(input int) string {
+	output := ""
+	if input%3 == 0	{
+		output += "Pling"
+	}
+	if input%5 == 0	{
+		output += "Plang"
+	}
+	if input%7 == 0	{
+		output += "Plong"
+	}
+	if (input%3 != 0 && input%5 != 0 && input%7 != 0)	{
+		output = strconv.Itoa(input)
+		// output = string(input)
+	}
+	return output  
+}
 
 func main() {
 	fmt.Println("Hello, world.")
+	test := ""
+	fmt.Println(test)
+	test += "more"
+	fmt.Println(test)
+	test += "MOAR"
+	fmt.Println(test)
 }
